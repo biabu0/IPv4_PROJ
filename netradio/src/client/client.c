5b51206f07b5983d4d1979fb63e48b4c071bb322
wyl <yluwa0007@gmail.com>
2024-10-23 20:58:02 +0800

    client_2

--- netradio/src/client/client.c
+++ netradio/src/client/client.c
@@ -1,6 +1,16 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<unistd.h>
+#include<errno.h>
+#include<arpa/inet.h>
+#include<getopt.h>
+#include<net/if.h>
 
+#include<proto.h>                   //有Makefile中指定地址
+
+#include"client.h"
 
 /*
  *  -M  --mgrop     指定多播组
@@ -17,19 +27,48 @@ struct client_conf_st client_conf = {\
 
 static void printhelp(void)
 {
-    printf("-P --port       指定接收端口\n");
+    printf("-P  --port      指定接收端口\n");
     printf("-M  --mgrop     指定多播组\n");
     printf("-p  --player    指定播放器 \n");
     printf("-H  --help      显示帮助 \n");
 }
 
+
+static ssize_t writen(int fd, const char *buf, size_t len)
+{
+    int ret;
+    int pos  = 0;
+    while(1){
+        ret = write(fd, buf+pos, len);
+        if(ret < 0){
+            if(errno == EINTR)
+                continue;
+            perror("write");
+            return -1;
+        }
+        len -= ret;
+        pos += ret;
+    }
+    
+    return pos;
+}
+
 int main(int argc, char **argv)
 {
+    pid_t pid;
+    int pd[2];
     int index = 0;
     int c;
+    int sd;
+    int val;
+    int chosenid;
+    struct ip_mreqn mreq;
+    struct sockaddr_in laddr, serveraddr,raddr;
+    socklen_t raddr_len;
+    socklen_t serveraddr_len;
     struct option argarr[] = {{"port", 1, NULL, 'P'},{"mgroup", 1, NULL, 'M'},\
                                 {"player", 1, NULL, 'p'},{"help", 0, NULL, 'H'},\
-                                {NULL, 0， NULL, 0}};
+                                {NULL, 0, NULL, 0}};
     /*初始化
      *级别：默认值，配置文件，环境变量，命令行参数（高）
      **/
@@ -57,15 +96,208 @@ int main(int argc, char **argv)
 
         }
     }
-    socket();
-    
-    pipe();
-    
-    fork();
+    sd = socket(AF_INET, SOCK_DGRAM, 0);                        //报的形式传输
+    if(sd < 0){
+        perror("socket()");
+        exit(1);
+    }
 
-    //子进程：调用解码器
+    //多播地址
+    inet_pton(AF_INET, client_conf.mgroup, &mreq.imr_multiaddr);
+    //本地地址
+    inet_pton(AF_INET, "0.0.0.0", &mreq.imr_address);
+    //当前网络设备索引号
+    mreq.imr_ifindex = if_nametoindex("ens33");        //使用ifconfig命令查看网卡；这里可以使用命令端指定
+    
+    //设置属性，加入多播组
+    if(setsockopt(sd, IPPROTO_IP, IP_ADD_MEMBERSHIP, &mreq, sizeof(mreq)) < 0){
+        perror("setsockopt()");
+        exit(1);
+    }
+    
+
+    //开启多播数据包的回环功能,当设置为1时，开启回环功能，发送者可以接收到自己发送的多播数据包。
+    val = 1;
+    if(setsockopt(sd, IPPROTO_IP, IP_MULTICAST_LOOP, &val, sizeof(val)) <  0){
+        perror("setsockopt()");
+        exit(1);
+    }
+
+
+/*
+    绑定"0.0.0.0"作为本地地址意味着该套接字将监听所有可用的网络接口上的多播数据包。这允许客户端从网络中的任何位置接收多播数据，而无需指定特定的网络接口或IP地址。
+*/
+    laddr.sin_family = AF_INET;
+    laddr.sin_port = htons(atoi(client_conf.rcvport));
+    inet_pton(AF_INET, "0.0.0.0", &laddr.sin_addr);
+
+    //绑定本地地址
+    if(bind(sd, (void *)&laddr, sizeof(laddr))<0){
+        perror("bind()");
+        exit(1);
+    }
+
+    if(pipe(pd) < 0){
+        perror("pipe()");
+        exit(1);
+    }
+
+    
+    pid = fork();
+    if(pid < 0){
+        perror("fork()");
+        exit(1);
+    }
+
+    //子进程调用解码器
+    if(pid == 0){
+
+        close(sd);                      //子进程中的sd关闭，不要泄露文件描述符
+        close(pd[1]);                   //子进程不需要管道的写端口
+        dup2(pd[0], 0);                  //管道的读端作为0号文件描述符，作为子进程的标准输入；播放的时候解码器只能获得标准输入的内容，使用的是"-"
+        if(pd[0] > 0)                   //本身不是标准输入
+            close(pd[0]);
+        
+        //子进程播放器选择
+        //使用shell程序执行
+        execl("/bin/sh", "sh", "-c", client_conf.player_cmd, NULL);     
+        perror("execl()");
+        exit(1);
+        
+    }
+
     //父进程：从网络收包，发送给子进程
     
+    //收节目单
+    struct msg_list_st *msg_list;
+
+    msg_list = malloc(MSG_LIST_MAX);        //最大的包
+    if(msg_list == NULL){
+        perror("malloc()");
+        exit(1);
+    }
+
+    int len;    
+    while(1)
+    {
+        len = recvfrom(sd, msg_list, MSG_LIST_MAX, 0, (void *)&serveraddr, &serveraddr_len);
+        if(len < sizeof(struct msg_list_st)){
+            fprintf(stderr, "message is too small.\n");
+            continue;
+        }
+        if(msg_list->chnid != LISTCHNID)   //不是节目单频道号
+        {
+            fprintf(stderr, "chnid is not match.\n");
+            continue;
+        }
+        break;              //收到节目单
+    }
+
+    //打印节目单并选择频道
+    struct msg_listentry_st *pos;
+    //
+    for(pos = msg_list->entry; (char *)pos < (((char *)msg_list) + len) ;pos = (void*)(((char *)pos) + ntohs(pos->len))){
+        printf("channel %d : %s\n", pos->chnid, pos->desc);
+    }
+
+    free(msg_list);                 //节目单已经打印出来，不在需要该结构体，释放
+    
+    //scanf放在循环中危险，如果输入‘a’，会放在缓冲区中取不出来，会一直在while中
+    
+    int ret;
+    while(1){
+        ret = scanf("%d", &chosenid);
+        if(ret != 1)
+            exit(1);                        //做为异常终止的一种
+    }
+
+    //收频道包，发送给子进程
+    
+    
+    struct msg_channel_st *msg_channel;
+
+    msg_channel = malloc(MSG_CHANNEL_MAX);
+    if(msg_channel == NULL){
+        perror("malloc()");
+        exit(1);
+    }
+    
+    while(1){
+        len = recvfrom(sd, msg_channel, MSG_CHANNEL_MAX, 0, (void *)&raddr, &raddr_len);
+        if(len < 0){
+            perror("recvfrom()");
+            exit(1);
+        }
+        //验证当前收到的频道包的发送方是否与收到节目包的发送方地址一样
+        if(raddr.sin_addr.s_addr != serveraddr.sin_addr.s_addr || raddr.sin_port != serveraddr.sin_port){
+            fprintf(stderr, "Ignore:address not match.\n");
+            continue;               //再给一次机会试一下
+        }
+
+        if(len < sizeof(struct msg_channel_st)){            //两个字节都不到（边长的结构体）
+            fprintf(stderr, "aIgnore: message too small.\n");
+            continue;
+        }
+
+        if(msg_channel->chnid == chosenid)
+        {
+            fprintf(stdout,"accepted msg:%d recieved.\n", msg_channel->chnid);
+            //writen函数，坚持写够多少个字节
+            if(writen(pd[1], msg_channel->data, len-sizeof(chnid_t)) < 0)
+                exit(1);
+        }
+        
+    }
+    
+    //可以使用信号来处理，将这些free操作和close操作放到信号处理函数中
+    free(msg_channel);
+    close(sd);
 
     exit(0);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- netradio/src/client/client.h
+++ netradio/src/client/client.h
@@ -3,18 +3,21 @@
 
 
 //给定播放器的地址，命令行上不输出各种提示则将其重定向到空设备上
-#define DEFAULT_PLAYERCMD   "/usr/bin/mpg123 > /dev/null"         //player没有必要给到server端
+// - :表示只接受标准输入来的内容
+#define DEFAULT_PLAYERCMD   "/usr/bin/mpg123 - > /dev/null"         //player没有必要给到server端
 
 
 //用户端可以指定的内容
 struct client_conf_st
 {
     char *rcvport;              //跨网络的数据传输不能出现char *，这里不能直接上传网络
-    char *mgropt;
-    char *player_amd;
+    char *mgroup;
+    char *player_cmd;
 };
 
 //extern 声明；全局变量可以在包含当前.h文件的其他.c中使用，拓展当前全局变量的作用域
+
 extern struct client_conf_st client_conf;       
+
 
 #endif
